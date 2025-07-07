750f208c65f3e9d8ce8475d7015e9f1aac51432f
Sean Larimore <slarimore02@gmail.com>
2025-07-07 15:11:15 +0200

    Update change-controller-password.sh

--- files/change-controller-password.sh
+++ files/change-controller-password.sh
@@ -1,26 +1,28 @@
 #!/bin/bash
-#DEFAULT_PASSWORD=${controller-default-password}
-#NEW_PASSWORD=${controller-password}
-#CONTROLLER_ADDRESS=${self.network_interface.0.access_config.0.nat_ip}
-CURRENT_PASSWORD=
+OLD_PASSWORD=
 NEW_PASSWORD=
 CONTROLLER_ADDRESS=
+AVI_VERSION=
+AVI_TENANT_NAME="admin"
 
 usage()
 {
-    echo "usage: change-controller-password.sh [[[--current-password password ] [--new-password password ] [--controller-address address]] | [--help]]"
+    echo "usage: change-controller-password.sh [[[--old-password password ] [--new-password password ] [--controller-address address] [--avi-version version]] | [--help]]"
 }
 
 while [ "$1" != "" ]; do
     case $1 in
-        --current-password )    shift
-                                CURRENT_PASSWORD=$1
+        --old-password )    shift
+                                OLD_PASSWORD=$1
                                 ;;
         --new-password ) shift  
                                 NEW_PASSWORD=$1
                                 ;;
         --controller-address )  shift  
                                 CONTROLLER_ADDRESS=$1
+                                ;;
+        --avi-version )  shift  
+                                AVI_VERSION=$1
                                 ;;
         -h | --help )           usage
                                 exit
@@ -34,20 +36,18 @@ done
 until $(curl -k -X GET --output /dev/null --silent --head --fail https://$CONTROLLER_ADDRESS); do
     sleep 10
 done
-# Login to the Controller with the default credentials and save the session cookies
-COOKIE=$(curl -k --silent --output /dev/null -c - --location --request POST "https://$CONTROLLER_ADDRESS/login" --form username="admin" --form password="$CURRENT_PASSWORD")
-# Setup CSRF Token Cookie
-TOKEN=$(echo $COOKIE |  grep -o -E '.csrftoken.{0,33}' | sed -e 's/^[ \t]*csrftoken[ \t]//')
-# Setup avi-sessionid Cookie
-SESSIONID=$(echo $COOKIE | grep -o -E '.avi-sessionid.{0,33}' | sed -e 's/^[ \t]*avi-sessionid[ \t]//')
+# Login to the Controller with the default credentials and save the cookie-jar
+curl -k -i -c ./cookie-jar -POST "https://$CONTROLLER_ADDRESS/login" \
+-H "X-Avi-Version: $AVI_VERSION" -H "X-Avi-Tenant: $AVI_TENANT_NAME" \
+-H "Content-Type: application/json" -d '{"username": "admin", "password": "'$OLD_PASSWORD'"}' 
+
+# Setup CSRF Token 
+AVI_CSRF_TOKEN=$(sed -nr "s/.*csrftoken\s+(.*)$/\1/p" ./cookie-jar)
+
 # Change Password
-curl -v -k --location --request PUT "https://$CONTROLLER_ADDRESS/api/useraccount" \
---header "x-csrftoken: $TOKEN" \
---header "referer: https://$CONTROLLER_ADDRESS/" \
---header 'Content-Type: application/json' \
---header "Cookie: csrftoken=$TOKEN; avi-sessionid=$SESSIONID; sessionid=$SESSIONID" \
---data "{
-\"username\": \"admin\",
-\"password\": \"$NEW_PASSWORD\",
-\"old_password\": \"$CURRENT_PASSWORD\"
-}"
+curl -v -k -i -b ./cookie-jar --location --request PUT "https://$CONTROLLER_ADDRESS/api/useraccount" \
+-H "X-CSRFToken: $AVI_CSRF_TOKEN" -H "Referer: https://$CONTROLLER_ADDRESS/" -H "X-Avi-Tenant: $AVI_TENANT_NAME" -H "Content-Type: application/json" \
+-d '{"username": "admin", "password": "'$NEW_PASSWORD'", "old_password": "'$OLD_PASSWORD'"}'
+
+#delete cookie-jar file
+rm -f ./cookie-jar
